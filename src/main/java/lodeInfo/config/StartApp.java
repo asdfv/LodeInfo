fc131d8d5145432964e9c32ac01eacb30c65a020
asdfv <grodno.by@mail.ru>
2016-04-02 14:48:27 +0300

    bljaaaa2

--- src/main/java/lodeInfo/config/StartApp.java
+++ src/main/java/lodeInfo/config/StartApp.java
@@ -7,9 +7,6 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories("lodeInfo")
-@EntityScan(basePackages = "lodeInfo")
-@ComponentScan("lodeInfo")
 public class StartApp {
 
     public static void main(String[] args) {
