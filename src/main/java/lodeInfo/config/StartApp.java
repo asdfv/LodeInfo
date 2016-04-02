4749cd17747147d0cd4ddff443f15bfecee793bc
asdfv <grodno.by@mail.ru>
2016-04-02 15:20:34 +0300

    ddd

--- src/main/java/lodeInfo/config/StartApp.java
+++ src/main/java/lodeInfo/config/StartApp.java
@@ -2,9 +2,6 @@ package lodeInfo.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.orm.jpa.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 public class StartApp {
