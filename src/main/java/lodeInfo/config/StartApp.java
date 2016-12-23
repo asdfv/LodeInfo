ade96cd33a4f32d7b895a70873c1407869e3fd5f
Vasili <grodno.by@mail.ru>
2016-12-23 14:46:23 +0300

    Remove second target from ignore

--- src/main/java/lodeInfo/config/StartApp.java
+++ src/main/java/lodeInfo/config/StartApp.java
@@ -5,8 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 @ComponentScan("lodeinfo")
@@ -16,6 +14,6 @@ public class StartApp {
 
     public static void main(String[] args) {
         SpringApplication.run(StartApp.class, args);
-
     }
 }
+
